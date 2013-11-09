7694de96fa58cf5e8e42c68ac0d9b6bf4d3b6455
vagrant <vagrant@precise32.(none)>
2013-11-09 09:10:41 +0000

    Start Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loanittome::Application.config.secret_key_base = '7023dfdff2999d9a1868115807c81942d25e8068333955fed59c7f579d84d2da8f5bb4e295f95ab51c2e1ff1f18c418f26dc4cedbaf276ca3fc1b8e8fc2b420b'
