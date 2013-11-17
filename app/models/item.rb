bd1256a31bc5769bc0688496a3362dd0614586db
Gusevv <vg@avatech.in>
2013-11-17 08:23:37 +0000

    change views, add column category for list Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
 	validates :title, presence: true;
 	belongs_to :category
+
+	def category_name
+		if category.present?
+			category.title
+		else
+			"Not set"
+		end
+	end
 end
